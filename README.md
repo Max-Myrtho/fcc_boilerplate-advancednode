6afa782b5b2900766f44e8d59f3da14d5106caf1
Max-Myrtho <hello@maxmyrtho.me>
2025-05-03 15:03:22 -0400

    all files added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# fcc_boilerplate-advancednode
+# Advanced Node and Express
+
+This is the boilerplate for the Advanced Node and Express lessons. Instructions for the lessons start at https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/
---
+++ server.js
@@ -0,0 +1,53 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const routes = require("./routes.js");
+const auth = require("./auth.js");
+
+const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+fccTesting(app); //For FCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+
+  io.on("connection", (socket) => {
+    console.log("A user has connected");
+  });
+
+  // Be sure to add this...
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("index", { title: e, message: "Unable to connect to database" });
+  });
+});
+// app.listen out here...
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
